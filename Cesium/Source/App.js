aefb377e81219083796d3df4b82d22ea0fdb4d23
YutaTsu <tsuney.762@gmail.com>
2019-08-06 22:56:36 +0900

    a

--- Cesium/Source/App.js
+++ Cesium/Source/App.js
@@ -1,9 +1,9 @@
- var viewer = new Cesium.Viewer('cesiumContainer');
-    var modelMatrix=Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(-35.665439,139.712533,200));
-    var entity = viewer.entities.add(Cesium.Model.fromGltf({
-    url:https//:yutatsu.github.io/AoyamaArchive/Cesium/Source/model.glb,
+var viewer = new Cesium.Viewer('cesiumContainer');
+var modelMatrix=Cesium.Transforms.eastNorthUpToFixedFrame(Cesium.Cartesian3.fromDegrees(-35.665439,139.712533,200));
+var entity = viewer.entities.add(Cesium.Model.fromGltf({
+    url:https://yutatsu.github.io/AoyamaArchive/Cesium/Source/model.glb,
     modelMatrix : modelMatrix,
     scale : 10.0
     }));
- var viewer.camera.flyTo({
+var viewer.camera.flyTo({
     destination : Cesium.Cartesian3.fromDegrees(-35.665439,139.712533,1000)});
